62ebe4d67b994414517b5889a40b42c4edd7f09c
Paul566 <arkhipov.pa@phystech.edu>
2021-05-28 15:44:25 +0300

    remove plotnet()

--- collect.py
+++ collect.py
@@ -66,49 +66,6 @@ def getnet():
         json.dump(queue, f)
 
 
-def plotnet(threshold):
-    with open('ids_to_names.json') as f:
-        ids_to_names = json.load(f)
-    with open('edges.json') as f:
-        list_edges = json.load(f)
-    edges = set()
-    for l in list_edges:
-        edges.add((l[0], l[1]))
-    list_ids = []
-    for key in ids_to_names:
-        list_ids.append(key)
-    g = igraph.Graph()
-    N = len(list_ids)
-    g.add_vertices(N)
-    for i in range(N):
-        g.vs[i]['label'] = ids_to_names[list_ids[i]]
-    for i in range(N):
-        for j in range(i, N):
-            if (list_ids[i], list_ids[j]) in edges:
-                g.add_edge(i, j)
-
-    nodes_to_delete = []
-    for i in range(N):
-        if g.vs[i].degree() < threshold:
-            nodes_to_delete.append(i)
-    g.delete_vertices(nodes_to_delete)
-
-    visual_style = {'vertex_size': 5,
-                    'vertex_label_size': 18,
-                    'vertex_label_dist': 3,
-                    'bbox': (1000, 1000),
-                    'margin': 100,
-                    'edge_curved': True}
-
-    plot = igraph.Plot(target='coauthorGraph.png', bbox=(1000, 1000), background="white")
-    plot.add(g, layout=g.layout_kamada_kawai(), **visual_style)
-    plot.redraw()
-    # ctx = cairo.Context(plot.surface)
-    # ctx.set_font_size(24)
-    # drawer = TextDrawer(ctx, title, halign=TextDrawer.CENTER)
-    # drawer.draw_at(0, 40, width=1000)
-    plot.save()
-
 
 if __name__ == '__main__':
     getnet()
